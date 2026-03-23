863496cbab47c788622403945a3cd535420254cb
mehdiobt <lilyasmarket4@gmail.com>
2026-03-23 01:16:56 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+// CabinReady Service Worker
+const CACHE = 'cabinready-v1';
+const FILES = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
